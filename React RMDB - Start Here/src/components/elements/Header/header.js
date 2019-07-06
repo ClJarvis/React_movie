5efd86746cf4c12dafa02832a197f6edd4317423
Chris <clj@ollure.com>
2019-07-05 20:19:56 -0500

    connecting routes from individual movies

--- React RMDB - Start Here/src/components/elements/Header/header.js
+++ React RMDB - Start Here/src/components/elements/Header/header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
 	return (
 			<div className="rmdb-header">
 				<div className="rmdb-Header-content">
-					<img className='rmbd-logo' src='./images/reactMovie_logo.png' alt='rmdb mlogo' />
+					<Link to="/">
+					<img className='rmbd-logo' src='./images/reactMovie_logo.png' alt='rmdb-mlogo' />
+					</Link>
 					<img className='rmbd-tmdb-logo' src="./images/tmdb_logo.png" alt='tmdb-logo' />
 				</div>
 			</div>
--- React RMDB - Start Here/src/components/elements/MovieThumb/MovieThumb.js
+++ React RMDB - Start Here/src/components/elements/MovieThumb/MovieThumb.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import './MovieThumb.css';
 
 const MovieThumb = (props) => {
 	return (
 		<div className="rmbd-moviethumb">
-			<img src={props.image} alt="MovieThumb" />
+			{props.clickable ?
+				<Link to={{ pathname: `/${props.movieId}`, movieName: `${props.movieName}` }}>
+				 <img src={props.image} alt="MovieThumb" />
+				</Link>
+				:
+				 <img src={props.image} alt="MovieThumb" />
+			}
 			
 		</div>
 		)
